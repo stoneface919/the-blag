67cb0c770f125be7c31020ae7e7d322861e26296
Saùl Zenteno <saulzenteno27@gmail.com>
2021-04-24 14:30:38 -0500

    Initial Simple Working Release

--- app.js
+++ app.js
@@ -2,22 +2,67 @@
 let express = require('express');
 let bodyParser= require('body-parser');
 let ejs = require('ejs');
+let lodash = require('lodash')
 server = express();
 server.use(bodyParser.urlencoded({extended:true}));
 server.use(express.static('public'));
 
+
 server.set('view engine', 'ejs')
-const homeContent = "Hemmp"
+const homeContent = "Server Content"
+const aboutContent = 'About Content'
+const contactContent = 'Contact Content'
 
-
+var posts = [ ]
 server.get('/', (req, res)=>{
-    res.render('main', {content: homeContent})
+    res.render('main', {posts: posts})
+   
+    
+})
+server.get('/posts/:postName' ,(req,res)=>{
+    console.log(req.params.postName)
+    for(post of posts) {
+        if (lodash.lowerCase(post.title) === lodash.lowerCase(req.params.postName)) {
+            res.render('post',{
+                title:post.title,
+                entrie:post.entrie
+            })
+    
+        }
+    }
+    
+
+})
+
+server.get('/about', (req,res)=>{
+    res.render('about', {content: aboutContent})
 })
 
 
-server.get
+server.get('/contact', (req,res)=>{
+    res.render('contact', {content: contactContent})
+})
 
 
+server.get('/compose', (req, res)=>{
+    res.render('compose')
+})
+server.post('/compose', (req,res)=>{ 
+    const post = {
+        title: req.body.postTitle ,
+        entrie: req.body.postEntry
+    }
+
+    posts.push(post)
+    res.redirect('/')
+    console.log(posts)
+    
+
+
+    // entries[req.body.postTitle] = req.body.postEntry
+    // console.log(entries)
+    // res.redirect('/compose')
+})
 
 
 server.listen(process.env.PORT || 3000, ()=>{
